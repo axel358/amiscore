ca1472a7a2841b95858703c71f534976eed940dd
Axel358 <edgar.serrano.9803@gmail.com>
2021-07-31 09:54:35 -0400

    Improved swipe support, broke filering :(

--- app/src/main/java/cu/daxyel/amiscore/adapters/DiagnosticsAdapter.java
+++ app/src/main/java/cu/daxyel/amiscore/adapters/DiagnosticsAdapter.java
@@ -8,58 +8,50 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.chauthai.swipereveallayout.SwipeRevealLayout;
-import com.chauthai.swipereveallayout.ViewBinderHelper;
-
 import java.util.ArrayList;
-
 import cu.daxyel.amiscore.R;
 import cu.daxyel.amiscore.db.DbDiagnostics;
 import cu.daxyel.amiscore.models.Diagnosis;
 
 
 public class DiagnosticsAdapter extends RecyclerView.Adapter<DiagnosticsAdapter.DiagnosticsViewHolder>implements Filterable {
-    ArrayList<Diagnosis> diagnosisArrayList;
-    private ArrayList<Diagnosis> diagnosisArrayListFilter;
-    private CustomFilter mFilter;
-    private final ViewBinderHelper viewBinderHelper = new ViewBinderHelper();
+    private ArrayList<Diagnosis> diagnosis;
     private DbDiagnostics dbDiagnostics;
+    private CustomFilter mFilter;
 
-    public DiagnosticsAdapter(ArrayList<Diagnosis> diagnosisArrayList,Context context) {
+    public DiagnosticsAdapter(ArrayList<Diagnosis> diagnosisArrayList, Context context) {
 
-        this.diagnosisArrayList=diagnosisArrayList;
-        this.diagnosisArrayListFilter=new ArrayList<>();
-        this.diagnosisArrayListFilter.addAll(diagnosisArrayList);
-        this.mFilter = new CustomFilter(DiagnosticsAdapter.this);
-        this.dbDiagnostics=new DbDiagnostics(context);
+        this.diagnosis = diagnosisArrayList;
+        this.mFilter = new CustomFilter();
+        this.dbDiagnostics = new DbDiagnostics(context);
+    }
+
+    public ArrayList<Diagnosis> getDiagnosis() {
+        return diagnosis;
     }
 
     @NonNull
     @Override
     public DiagnosticsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-       View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.entry_diagnosis,null,false);
-       return new DiagnosticsViewHolder(view);
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.entry_diagnosis, null, false);
+        return new DiagnosticsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DiagnosticsViewHolder holder, int position) {
-        String txt=diagnosisArrayListFilter.get(position).getDisease()+" , "+ diagnosisArrayListFilter.get(position).getProbabilityInfo();
-        viewBinderHelper.setOpenOnlyOne(true);
-        viewBinderHelper.bind(holder.swipeRevealLayout, diagnosisArrayListFilter.get(position).getCi());
-        holder.nameTv.setText(diagnosisArrayListFilter.get(position).getName());
-        holder.patient_id.setText(diagnosisArrayListFilter.get(position).getCi());
-        holder.diagnosis_date.setText(diagnosisArrayListFilter.get(position).getDate());
+        String txt=diagnosis.get(position).getDisease() + " , " + diagnosis.get(position).getProbabilityInfo();
+        holder.nameTv.setText(diagnosis.get(position).getName());
+        holder.patient_id.setText(diagnosis.get(position).getCi());
+        holder.diagnosis_date.setText(diagnosis.get(position).getDate());
         holder.disease.setText(txt);
 
     }
 
     @Override
     public int getItemCount() {
-        return diagnosisArrayListFilter.size();
+        return diagnosis.size();
     }
 
     @Override
@@ -67,72 +59,44 @@ public class DiagnosticsAdapter extends RecyclerView.Adapter<DiagnosticsAdapter.
         return mFilter;
     }
 
-    public class DiagnosticsViewHolder extends RecyclerView.ViewHolder {
-        private TextView nameTv,patient_id,disease,diagnosis_date;
-        private SwipeRevealLayout swipeRevealLayout;
-        private ImageView Edit, Delete;
-        public DiagnosticsViewHolder(@NonNull View itemView) {
-            super(itemView);
-             nameTv = itemView.findViewById(R.id.diagnosis_name_tv);
-             patient_id = itemView.findViewById(R.id.diagnosis_patient_id);
-             disease = itemView.findViewById(R.id.diagnosis_disease);
-             diagnosis_date = itemView.findViewById(R.id.diagnosis_date);
-
-             Edit = itemView.findViewById(R.id.edit);
-             Delete =itemView.findViewById(R.id.delete);
-             swipeRevealLayout=itemView.findViewById(R.id.swipe);
-
-             Delete.setOnClickListener(new View.OnClickListener() {
-                 @Override
-                 public void onClick(View view) {
-                     int selectedEntryLayotPosition = getAdapterPosition();
-                     TextView ci = itemView.findViewById(R.id.diagnosis_patient_id);
-                     Diagnosis diagnosisSelected = new Diagnosis(0, "", "", "", "", "");
-                     int selectedEntryId = 0;
-                     for (Diagnosis diagnosis : diagnosisArrayListFilter) {
-                         if (diagnosis.getCi().equals((ci.getText()).toString())) {
-                             selectedEntryId = diagnosis.getId();
-                             diagnosisSelected = diagnosis;
-                         }
-                     }
-                     dbDiagnostics.deleteDiagnostic(selectedEntryId);
-                     diagnosisArrayListFilter.remove(diagnosisSelected);
-                     notifyItemRemoved(selectedEntryLayotPosition);
-                 }
-             });
-        }
-    }
-
-    public class CustomFilter extends Filter {
-        private DiagnosticsAdapter diagnosticsAdapter;
-
-        private CustomFilter(DiagnosticsAdapter diagnosticsAdapter) {
-            super();
-            this.diagnosticsAdapter = diagnosticsAdapter;
-        }
-
+    class CustomFilter extends Filter {
         @Override
         protected FilterResults performFiltering(CharSequence constraint) {
-           diagnosisArrayListFilter.clear();
-            final FilterResults results = new FilterResults();
+
+            FilterResults results = new FilterResults();
+            ArrayList<Diagnosis> resultsList=new ArrayList<Diagnosis>();
+
             if (constraint.length() == 0) {
-                diagnosisArrayListFilter.addAll(diagnosisArrayList);
+                results.values = diagnosis;
+                results.count = diagnosis.size();    
             } else {
-                final String filterPattern = constraint.toString().toLowerCase().trim();
-                for (final Diagnosis diagnosis : diagnosisArrayList) {
-                    if (diagnosis.getName().toLowerCase().contains(filterPattern)) {
-                        diagnosisArrayListFilter.add(diagnosis);
+                for (final Diagnosis ddiagnosis : diagnosis) {
+                    if (ddiagnosis.getName().toLowerCase().contains(constraint.toString().toLowerCase().trim())) {
+                        resultsList.add(ddiagnosis);
                     }
                 }
+                results.values = resultsList;
+                results.count = resultsList.size();
             }
-            results.values = diagnosisArrayListFilter;
-            results.count = diagnosisArrayListFilter.size();
             return results;
         }
 
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
-            this.diagnosticsAdapter.notifyDataSetChanged();
+            notifyDataSetChanged();
         }
     }
+
+
+    public class DiagnosticsViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameTv,patient_id,disease,diagnosis_date;
+        public DiagnosticsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTv = itemView.findViewById(R.id.diagnosis_name_tv);
+            patient_id = itemView.findViewById(R.id.diagnosis_patient_id);
+            disease = itemView.findViewById(R.id.diagnosis_disease);
+            diagnosis_date = itemView.findViewById(R.id.diagnosis_date);
+        }
+    }
+
 }
--- app/src/main/java/cu/daxyel/amiscore/fragments/ManageDiagnosticsFragment.java
+++ app/src/main/java/cu/daxyel/amiscore/fragments/ManageDiagnosticsFragment.java
@@ -1,28 +1,41 @@
 package cu.daxyel.amiscore.fragments;
 
-import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.snackbar.Snackbar;
 import cu.daxyel.amiscore.R;
 import cu.daxyel.amiscore.adapters.DiagnosticsAdapter;
 import cu.daxyel.amiscore.db.DbDiagnostics;
-import cu.daxyel.amiscore.models.Diagnosis;;
-import android.widget.ArrayAdapter;
-import android.content.Context;
+import cu.daxyel.amiscore.models.Diagnosis;
 import java.util.ArrayList;
-import android.widget.TextView;
-import android.os.AsyncTask;
-import android.widget.ProgressBar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import androidx.appcompat.widget.SearchView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import android.widget.Spinner;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.AdapterView;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class ManageDiagnosticsFragment extends Fragment {
     private RecyclerView diagnosisRv;
@@ -32,7 +45,6 @@ public class ManageDiagnosticsFragment extends Fragment {
     private Context context;
     private SearchView searchView;
     private DiagnosticsAdapter diagnosticsAdapter;
-    private ArrayList<Diagnosis> results;
     private String index;
     private boolean show_load;
 
@@ -65,22 +77,133 @@ public class ManageDiagnosticsFragment extends Fragment {
         indexSpinner.setAdapter(new ArrayAdapter<String>(context, R.layout.entry_index_spnr, inexes));
         indexSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
+                @Override
+                public void onItemSelected(AdapterView<?> p1, View p2, int p3, long p4) {
+                    index = p1.getItemAtPosition(p3).toString();
+                    show_load = true;
+                    if (index.equalsIgnoreCase("all")) {
+                        new LoadDiagnosisTask().execute();
+                    } else {
+                        new LoadDiagnosisTask().execute(index);
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> p1) {
+                }
+            });
+
+        ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
+
+            private Bitmap editIcon=getBitmapFromVector(R.drawable.ic_edit);
+            private Bitmap deletIcon=getBitmapFromVector(R.drawable.ic_delete);
+            private Paint paint = new Paint();
             @Override
-            public void onItemSelected(AdapterView<?> p1, View p2, int p3, long p4) {
-                index = p1.getItemAtPosition(p3).toString();
-                show_load = true;
-                if (index.equalsIgnoreCase("all")) {
-                    new LoadDiagnosisTask().execute();
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+            
+
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int direction) {
+
+                final int position = viewHolder.getAdapterPosition();
+                if (direction == ItemTouchHelper.RIGHT) {
+                    diagnosticsAdapter.notifyItemChanged(position);
                 } else {
-                    new LoadDiagnosisTask().execute(index);
+                    final Diagnosis diagnosis = diagnosticsAdapter.getDiagnosis().get(viewHolder.getAdapterPosition());
+                    diagnosticsAdapter.getDiagnosis().remove(position);
+                    diagnosticsAdapter.notifyItemRemoved(position);
+
+                    Snackbar.make(diagnosisRv, "Item removed", Snackbar.LENGTH_LONG).setAction("Undo", new OnClickListener(){
+
+                            @Override
+                            public void onClick(View p1) {
+                                diagnosticsAdapter.getDiagnosis().add(position, diagnosis);
+                                diagnosticsAdapter.notifyItemInserted(position);
+                            }
+                        }).setCallback(new Snackbar.Callback(){ 
+                            @Override
+                            public void onDismissed(Snackbar snackbar, int event) {
+                                //Its gone, remove it from db
+                            }
+                        }).show();
                 }
             }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> p1) {
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+                int top = dpToPx(32) + editIcon.getWidth();
+                if (dX < 0) {
+                    paint.setColor(context.getResources().getColor(R.color.color_delete));
+
+                    c.drawRect((float) itemView.getRight() + dX, (float) itemView.getTop(),
+                               (float) itemView.getRight(), (float) itemView.getBottom(), paint);
+
+                    c.drawBitmap(deletIcon,
+                                 (float) itemView.getRight() - dpToPx(16) - deletIcon.getWidth(),
+                                 (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - deletIcon.getHeight()) / 2,
+                                 paint);
+
+                } else if (dX > 0) {
+
+
+                    if (dX >= top) {
+                        dX = top;
+                        Toast.makeText(context, "Yahoo", Toast.LENGTH_SHORT).show();
+                        itemView.setOnTouchListener(new OnTouchListener(){
+
+                                @Override
+                                public boolean onTouch(View p1, MotionEvent p2) {
+                                    if (p2.getAction() == MotionEvent.ACTION_UP) {
+                                        Toast.makeText(context, "Yahoo", Toast.LENGTH_SHORT).show();
+                                    }
+                                    return false;
+                                }
+                            });
+                    }
+
+                    paint.setColor(context.getResources().getColor(R.color.color_edit));
+
+                    c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
+                               (float) itemView.getBottom(), paint);
+
+                    c.drawBitmap(editIcon,
+                                 (float) itemView.getLeft() + dpToPx(16),
+                                 (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - editIcon.getHeight()) / 2,
+                                 paint);
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, ItemTouchHelper.ACTION_STATE_IDLE, isCurrentlyActive);
+                }
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
             }
-        });
+
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
+        itemTouchHelper.attachToRecyclerView(diagnosisRv);
+
+
+
         return view;
+    }
+
+    public int dpToPx(int dp) {
+        return (int) (dp * context.getResources().getDisplayMetrics().density + 0.5f);
+    }
+
+    public Bitmap getBitmapFromVector(int resid) {
+        Drawable vector = ContextCompat.getDrawable(context, resid);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+            vector = DrawableCompat.wrap(vector).mutate();
+
+        Bitmap bitmap = Bitmap.createBitmap(vector.getIntrinsicWidth(), vector.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vector.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        vector.draw(canvas);
+
+        return bitmap;
     }
 
     @Override
@@ -89,20 +212,20 @@ public class ManageDiagnosticsFragment extends Fragment {
         searchView = (SearchView) menu.findItem(R.id.menu_search_diagnosis).getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
-            @Override
-            public boolean onQueryTextSubmit(String p1) {
-                return false;
-            }
+                @Override
+                public boolean onQueryTextSubmit(String p1) {
+                    return false;
+                }
 
-            @Override
-            public boolean onQueryTextChange(String p1) {
-                if (p1.length() > 0)
-                    diagnosticsAdapter.getFilter().filter(p1);
-                else if (p1.length() == 0)
-                    diagnosticsAdapter.getFilter().filter("");
-                return true;
-            }
-        });
+                @Override
+                public boolean onQueryTextChange(String p1) {
+                    if (p1.length() > 0)
+                        diagnosticsAdapter.getFilter().filter(p1);
+                    else if (p1.length() == 0)
+                        diagnosticsAdapter.getFilter().filter("");
+                    return true;
+                }
+            });
     }
 
     class LoadDiagnosisTask extends AsyncTask<String, Void, ArrayList<Diagnosis>> {
@@ -130,7 +253,7 @@ public class ManageDiagnosticsFragment extends Fragment {
             super.onPostExecute(result);
             loadingTv.setVisibility(View.GONE);
             loadingPb.setVisibility(View.GONE);
-            diagnosticsAdapter = new DiagnosticsAdapter(result,context);
+            diagnosticsAdapter = new DiagnosticsAdapter(result, context);
             diagnosisRv.setAdapter(diagnosticsAdapter);
         }
     }
