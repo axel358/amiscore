32bdc1344b21ef960f3501dd6aca46d0d97de276
Axel358 <edgar.serrano.9803@gmail.com>
2022-01-26 07:02:19 -0500

    Allow to change between login and signup fragments

--- app/src/main/java/cu/daxyel/amiscore/fragments/CreateAccountFragment.java
+++ app/src/main/java/cu/daxyel/amiscore/fragments/CreateAccountFragment.java
@@ -5,12 +5,22 @@ import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import cu.daxyel.amiscore.R;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class CreateAccountFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_createaccount, container, false);
+        Button b = view.findViewById(R.id.switch_sign_in_btn);
+        b.setOnClickListener(new OnClickListener(){
+
+                @Override
+                public void onClick(View p1) {
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new LoginFragment()).commit();
+                }
+            });
         return view;
     }
 
--- app/src/main/java/cu/daxyel/amiscore/fragments/LoginFragment.java
+++ app/src/main/java/cu/daxyel/amiscore/fragments/LoginFragment.java
@@ -5,13 +5,24 @@ import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import cu.daxyel.amiscore.R;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_loginfragment, container, false);
+        Button b = view.findViewById(R.id.switch_sign_up_btn);
+        b.setOnClickListener(new OnClickListener(){
+
+                @Override
+                public void onClick(View p1) {
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new CreateAccountFragment()).commit();
+                }
+            });
         return view;
     }
+  
 
 }
